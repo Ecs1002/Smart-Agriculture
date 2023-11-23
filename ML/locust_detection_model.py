941339af4e91cbfa615b326d45aab00b5cf5284e
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-11-24 00:44:59 +0530

    HOG feats

--- ML/locust_detection_model.py
+++ ML/locust_detection_model.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import classification_report
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split
-import matplotlib.image as mpimg
+from sklearn.model_selection import GridSearchCV, train_test_split
+from skimage.feature import hog
+from skimage import exposure
 import joblib
 
 # System libraries
@@ -16,7 +16,7 @@ import os.path
 
 BATCH_SIZE = 32
 IMAGE_SIZE = (224, 224)
-dataset_path = r'C:\Users\deepa\Downloads\locust_pics'
+dataset_path = '/kaggle/input/pest-normalized'
 
 image_dir = Path(dataset_path)
 
@@ -31,8 +31,6 @@ if not filepaths:
 else:
     labels = list(map(lambda x: os.path.split(os.path.split(x)[0])[1], filepaths))
 
-labels = list(map(lambda x: os.path.split(os.path.split(x)[0])[1], filepaths))
-
 # Convert filepaths and labels to pandas Series
 print("Converting filepaths and labels to pandas Series...")
 filepaths_series = pd.Series(filepaths, name='Filepath')
@@ -42,10 +40,6 @@ print("Filepaths and labels converted to pandas Series successfully.")
 # Concatenate filepaths and labels
 print("Concatenating filepaths and labels...")
 image_df = pd.concat([filepaths_series, labels_series], axis=1)
-
-# print(len(list(image_dir.glob(r'**/*.jpeg'))))
-
-image_df
 print("Filepaths and labels concatenated successfully.")
 
 # Display 16 pictures of the dataset with their labels
@@ -74,94 +68,60 @@ print("Saving the LabelEncoder...")
 joblib.dump(label_encoder, 'label_encoder.pkl')
 print("LabelEncoder saved successfully.")
 
-# Prepare features and labels
-print("Preparing features and labels...")
-X_train = np.array([np.array(Image.open(filepath).convert('RGB').resize(IMAGE_SIZE)) for filepath in train_df['Filepath']])
-y_train = train_df['Label']
+# Function to extract HOG features from an image
+def extract_hog_features(image_path):
+    img = Image.open(image_path).convert('L')  # Convert to grayscale
+    img = img.resize(IMAGE_SIZE)
+    img_array = np.array(img)
+    
+    # Calculate HOG features and return flattened array
+    features, _ = hog(img_array, pixels_per_cell=(8, 8), cells_per_block=(1, 1), visualize=True)
+    return features
 
-X_test = np.array([np.array(Image.open(filepath).resize(IMAGE_SIZE)) for filepath in test_df['Filepath']])
-print("Features and labels prepared successfully.")
+# Extract HOG features for training data
+print("Extracting HOG features for training data...")
+X_train = np.array([extract_hog_features(filepath) for filepath in train_df['Filepath']])
+print("HOG features extracted for training data.")
+
+# Extract HOG features for test data
+print("Extracting HOG features for test data...")
+X_test = np.array([extract_hog_features(filepath) for filepath in test_df['Filepath']])
+print("HOG features extracted for test data.")
 
 # Define the parameter grid
 param_grid = {
-    'n_estimators': [100, 200, 300, 400, 500],
-    'max_depth': [None, 10, 20, 30, 40, 50],
+    'n_estimators': [100, 200, 300],
+    'max_depth': [None, 10, 20, 30],
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4],
 }
 
-# Create a RandomForestClassifier
-print("Creating a RandomForestClassifier...")
-model = RandomForestClassifier(n_estimators=100)
-print("RandomForestClassifier created successfully.")
+# Create a Random Forest classifier
+print("Creating a Random Forest classifier...")
+model = RandomForestClassifier()
+print("Random Forest classifier created successfully.")
 
 # Create the GridSearchCV object
 grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, n_jobs=-1)
 
 # Fit the GridSearchCV object to the data
-grid_search.fit(X_train.reshape(X_train.shape[0], -1), y_train)
+print("Fitting GridSearchCV object to the data...")
+grid_search.fit(X_train, train_df['Label'])
+print("GridSearchCV object fitted successfully.")
 
 # Print the best parameters
 print(grid_search.best_params_)
 
-# Train the model
-print("Training the model...")
-model.fit(X_train.reshape(X_train.shape[0], -1), y_train)
-print("Model trained successfully.")
-
 # Evaluate the model
 print("Evaluating the model...")
-y_pred = model.predict(X_test.reshape(X_test.shape[0], -1))
+y_pred = grid_search.predict(X_test)
 
 y_test = test_df['Label']  # Define y_test
 
 print(classification_report(y_test, y_pred))
 print("Model evaluated successfully.")
 
-# Flatten the data
-X_train_flat = X_train.reshape(X_train.shape[0], -1)
-
-# Fit the GridSearchCV object to the data
-grid_search.fit(X_train_flat, y_train)
-
-if os.access(os.getcwd(), os.W_OK):
-    print("You have write permission for this directory.")
-else:
-    print("You do not have write permission for this directory.")
-
-# Display results
-print("Displaying results...")
-random_index = np.random.randint(0, len(test_df) - 1, 25)
-fig, axes = plt.subplots(nrows=5, ncols=5, figsize=(25, 20),
-                        subplot_kw={'xticks': [], 'yticks': []})
-
-for i, ax in enumerate(axes.flat):
-    image = Image.open(test_df.Filepath.iloc[random_index[i]])
-    ax.imshow(image)
-    if test_df.Label.iloc[random_index[i]] == y_pred[random_index[i]]:
-        color = "green"
-    else:
-        color = "red"
-    ax.set_title(f"True: {test_df.Label.iloc[random_index[i]]}\nPredicted: {y_pred[random_index[i]]}", color=color)
-plt.show()
-plt.tight_layout()
-print("Results displayed successfully.")
-
-def predict_insect(image_path):
-    # Load and preprocess the image
-    img = Image.open(image_path).convert('RGB').resize((224, 224))  # resize to (224, 224)
-    img_array = np.array(img).reshape(1, -1)
-
-    # Use the model to predict the class of the image
-    print("Predicting the class of the image...")
-    pred = model.predict(img_array)
-
-    # Convert the predicted class back to the original label
-    pred_label = label_encoder.inverse_transform(pred)
-
-    return pred_label[0]
-
 # Save the model
 print("Saving the model...")
-filename = joblib.dump(model, 'model.pkl')
+filename = joblib.dump(grid_search, 'model.pkl')
 print(f"Model saved as {filename}")
