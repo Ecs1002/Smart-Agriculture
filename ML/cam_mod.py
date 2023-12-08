df4fe3ed3429b24f312ec0d503378af73ea879b2
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-12-08 20:26:35 +0530

    updated cam_mod.py
    Since we are no longer using HOG, I've updated the code accordingly.

--- ML/cam_mod.py
+++ ML/cam_mod.py
@@ -1,45 +1,42 @@
+import io
 import time
 import picamera
+import RPi.GPIO as GPIO
+import joblib
 from PIL import Image
 import numpy as np
-import joblib
-from skimage.feature import hog
-import RPi.GPIO as GPIO
 
-# Load your machine learning model here
+# Load the trained model and label encoder
 model = joblib.load('model.pkl')
+label_encoder = joblib.load('label_encoder.pkl')
 
-# Set GPIO mode and pin
-GPIO.setmode(GPIO.BOARD)
-dispenser_pin = 17  # Replace with your actual GPIO pin
+# Define the predict_insect function
+def predict_insect(image):
+    # Preprocess the image
+    img = image.convert('RGB').resize((224, 224))  # resize to (224, 224)
+    img_array = np.array(img).reshape(1, -1)
 
-# Set up dispenser pin as output
+    # Use the model to predict the class of the image
+    pred = model.predict(img_array)
+
+    # Convert the predicted class back to the original label
+    pred_label = label_encoder.inverse_transform(pred)
+    
+    return pred_label[0]
+
+# Define the dispenser pin
+dispenser_pin = 18
+
+# Set up the GPIO
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(dispenser_pin, GPIO.OUT)
 
-# Function to extract HOG features from an image
-def extract_hog_features(img):
-    img = img.convert('L')  # Convert to grayscale
-    img = img.resize((224, 224))
-    img_array = np.array(img)
-    
-    # Calculate HOG features and return flattened array
-    features, _ = hog(img_array, pixels_per_cell=(8, 8), cells_per_block=(1, 1), visualize=True)
-    return features
-
-def detect_locust(image):
-    # Extract HOG features
-    features = extract_hog_features(image)
-    # Predict using the model
-    prediction = model.predict([features])
-    # Return True if locust is detected, False otherwise
-    return prediction[0] == 1
-
-def dispense_liquid():
-    GPIO.output(dispenser_pin, GPIO.HIGH)
-    print("Dispensing liquid...")
-    time.sleep(5)  # Adjust the time as needed
-    GPIO.output(dispenser_pin, GPIO.LOW)
-    print("Dispensing complete.")
+# Start dispensing
+GPIO.output(dispenser_pin, GPIO.HIGH)
+print("Dispensing liquid...")
+time.sleep(5)  # Adjust the time as needed
+GPIO.output(dispenser_pin, GPIO.LOW)
+print("Dispensing complete.")
 
 with picamera.PiCamera() as camera:
     camera.start_preview()
@@ -51,11 +48,8 @@ with picamera.PiCamera() as camera:
             stream.seek(0)
             image = Image.open(stream)
 
-            if detect_locust(image):
+            if predict_insect(image) == 'locust':
                 camera.capture('/home/pi/Desktop/image%s.jpg' % i)
                 print("Locust detected. Image captured.")
-                dispense_liquid()
-            time.sleep(2)
     finally:
         camera.stop_preview()
-        GPIO.cleanup()  # Clean up GPIO state
